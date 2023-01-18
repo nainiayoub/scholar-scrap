1b373f69d9f6d1e11d621835480cf38e3937df02
Ayoub NAINIA <nainiaayoub6@gmail.com>
2023-01-18 19:52:42 +0100

    app files

---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import requests
+import urllib
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import time
+from time import sleep
+
+from function import get_paperinfo, get_tags, get_papertitle, get_citecount, get_link, get_author_year_publi_info, cite_number
+
+# title
+st.markdown("""
+# Scholar Scrap
+Scraping relevant information of research papers from Google Scholar.
+""")
+
+# scraping function
+# creating final repository
+paper_repos_dict = {
+                    'Paper Title' : [],
+                    'Year' : [],
+                    'Author' : [],
+                    'Citation' : [],
+                    'Publication' : [],
+                    'Url of paper' : [] }
+
+# adding information in repository
+def add_in_paper_repo(papername,year,author,cite,publi,link):
+  paper_repos_dict['Paper Title'].extend(papername)
+  paper_repos_dict['Year'].extend(year)
+  paper_repos_dict['Author'].extend(author)
+  paper_repos_dict['Citation'].extend(cite)
+  paper_repos_dict['Publication'].extend(publi)
+  paper_repos_dict['Url of paper'].extend(link)
+  for i in paper_repos_dict.keys():
+    print(i,": ", len(paper_repos_dict[i]))
+    print(paper_repos_dict[i])
+  df = pd.DataFrame(paper_repos_dict)
+  
+  return df
+
+# headers
+headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+url_begin = 'https://scholar.google.com/scholar?start={}&q='
+url_end = '&hl=en&as_sdt=0,5='
+# input
+text_input = st.text_input("Search in Google Scholar", placeholder="What are you looking for?")
+total_to_scrap = st.slider("How many pages to scrap?", min_value=1, max_value=5, step=1, value=2)
+# create scholar url
+if text_input:
+    text_formated = "+".join(text_input.split())
+    input_url = url_begin+text_formated+url_end
+    if input_url:
+        response=requests.get(input_url,headers=headers)
+        # st.info(input_url)
+        total_papers = 10 * total_to_scrap
+        for i in range (0,total_papers,10):
+            # get url for the each page
+            url = input_url.format(i)
+            # function for the get content of each page
+            doc = get_paperinfo(url, headers)
+
+            # function for the collecting tags
+            paper_tag,cite_tag,link_tag,author_tag = get_tags(doc)
+
+            # paper title from each page
+            papername = get_papertitle(paper_tag)
+
+            # year , author , publication of the paper
+            year , publication , author = get_author_year_publi_info(author_tag)
+
+            # cite count of the paper 
+            cite = get_citecount(cite_tag)
+
+            # url of the paper
+            link = get_link(link_tag)
+
+            # add in paper repo dict
+            final = add_in_paper_repo(papername,year,author,cite,publication,link)
+
+            # use sleep to avoid status code 429
+            # sleep(10)
+        final['Year'] = final['Year'].astype('int')
+        final['Citation'] = final['Citation'].apply(cite_number).astype('int')
+        st.dataframe(final)
---
+++ function.py
@@ -0,0 +1,99 @@
+import streamlit as st
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# this function for the getting inforamtion of the web page
+def get_paperinfo(paper_url, headers):
+
+  #download the page
+  response=requests.get(paper_url,headers=headers)
+
+  # check successful response
+  if response.status_code != 200:
+    print('Status code:', response.status_code)
+    raise Exception('Failed to fetch web page ')
+
+  #parse using beautiful soup
+  paper_doc = BeautifulSoup(response.text,'html.parser')
+  for div in paper_doc.find_all("div", {'class':'gs_ggs gs_fl'}): 
+    div.decompose()
+
+  return paper_doc
+
+# this function for the extracting information of the tags
+def get_tags(doc):
+  paper_tag = doc.select('[data-lid]')
+  # cite_tag = doc.select('[title=Cite] + a')
+  cite_tag = doc.find_all('div', {"class": "gs_fl"})
+  link_tag = doc.find_all('h3',{"class" : "gs_rt"})
+  author_tag = doc.find_all("div", {"class": "gs_a"})
+
+  return paper_tag,cite_tag,link_tag,author_tag
+
+# it will return the title of the paper
+def get_papertitle(paper_tag):
+  
+  paper_names = []
+  
+  for tag in paper_tag:
+    paper_names.append(tag.select('h3')[0].get_text())
+
+  return paper_names
+
+# it will return the number of citation of the paper
+def get_citecount(cite_tag):
+  cite_count = []
+  for i in cite_tag:
+    cite = i.text
+    tmp = re.findall('Cited by[ ]\d+', cite)
+    if tmp:
+      cite_count.append(tmp[0])
+    else:
+      cite_count.append(0)
+
+  return cite_count
+
+# function for the getting link information
+def get_link(link_tag):
+
+  links = []
+
+  for i in range(len(link_tag)) :
+    if link_tag[i].a:  
+      links.append(link_tag[i].a['href']) 
+    else:
+      links.append(None)
+
+  return links 
+
+# function for the getting autho , year and publication information
+def get_author_year_publi_info(authors_tag):
+  years = []
+  publication = []
+  authors = []
+  for i in range(len(authors_tag)):
+      authortag_text = (authors_tag[i].text).split()
+      # year = int(re.search(r'\d+', authors_tag[i].text).group())
+      # years.append(year)
+      datesearch = re.findall("20\d+", authors_tag[i].text)
+      if len(datesearch) > 0:
+        year = int(datesearch[0])
+        years.append(year)
+      else:
+        year = 0
+        years.append(year)
+      publication.append(authortag_text[-1])
+      author = authortag_text[0] + ' ' + re.sub(',','', authortag_text[1])
+      authors.append(author)
+  
+  return years , publication, authors
+
+def cite_number(text):
+    result = text.split()[-1]
+    return result
+
+
+
